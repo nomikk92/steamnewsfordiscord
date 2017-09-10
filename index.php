fda9669d14f1d4fb20cf41552bfd33da82f18711
nomikk92 <31822239+nomikk92@users.noreply.github.com>
2017-09-10 20:47:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,54 +1,30 @@
-<?php
-require_once(dirname(__FILE__)."/view.php");
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta property="og:title" content="PTT Steam @Discord" />
+public void SendToDiscord(const char[] message) {
+    Handle request = SteamWorks_CreateHTTPRequest(k_EHTTPMethodPOST, "https://discordapp.com/api/webhooks/356427830550069251/GiczEYfFOsyEYTgSfvGWQnsYwx0Xx51tdlp4bOUGwoQ_LwxuuvpOstSHb0PDuaTv9XdI");
+    
+    SteamWorks_SetHTTPRequestGetOrPostParameter(request, "content", message);
+    SteamWorks_SetHTTPRequestHeaderValue(request, "Content-Type", "application/x-www-form-urlencoded");
+    
+    if(request == null || !SteamWorks_SetHTTPCallbacks(request, Discord_Callback) || !SteamWorks_SendHTTPRequest(request)) {
+        PrintToServer("[SendToSlack] SendToDiscord failed to fire");
+        delete request;
+    }
+}
 
-	<title>PTT Steam @Discord</title>
+public Discord_Callback(Handle hRequest, bool bFailure, bool bRequestSuccessful, EHTTPStatusCode eStatusCode) {
+    if(!bFailure && bRequestSuccessful) {
+        switch (eStatusCode) {
+            case 200:{
+                //all gud
+            }
+            default: {
+                PrintToServer("[Send To Discord] failed with code [%i]", eStatusCode);
+                SteamWorks_GetHTTPResponseBodyCallback(hRequest, Print_Response);
+            }
+        }
+    }
+    delete hRequest;
+}
 
-	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" href="bootstrap/css/bootstrap-flex.min.css">
-	<link rel="stylesheet" href="bootstrap/css/bootstrap-grid.min.css">
-	<link rel="stylesheet" href="bootstrap/css/bootstrap-reboot.min.css">
-	<link rel="stylesheet" href="css/index.css">
-
-	<script src="js/jquery-3.1.1.min.js"></script>
-	<script src="bootstrap/js/tether.min.js"></script>
-	<script src="bootstrap/js/bootstrap.min.js"></script>
-	<script src="js/index.js"></script>
-
-	<!--[if lt IE 9]>
-	  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-</head>
-<body>
-	<div class="container">
-		<div class="section">
-			<div class="row">
-				<div id="siteTitle" class="col-md-12"><h1>PTT Steam @Discord</h1></div>
-			</div>
-		</div>
-		<?php
-		if ((isset($_POST['loginPW']) && (USER_PASSWORD === $_POST['loginPW'])) ||
-			(isset($_POST['loginPW']) && (ADMIN_PASSWORD === $_POST['loginPW']))
-		) {
-			$isAdmin = (isset($_POST['loginPW']) && (ADMIN_PASSWORD === $_POST['loginPW']));
-			require_once(dirname(__FILE__)."/template/anonymous_message.php");
-			if ($isAdmin)
-			{
-				require_once(dirname(__FILE__)."/template/anonymous_log.php");
-			}
-		} else {
-			$isLogin = isset($_POST['loginPW']);
-			require_once(dirname(__FILE__)."/template/login.php");
-		}
-		?>
-	</div>
-</body>
-</html>
+public Print_Response(const char[] sData) {
+    PrintToServer("[Print_Response] %s", sData);
+}
